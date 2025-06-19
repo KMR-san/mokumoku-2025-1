19a234c08f9e1807d9012b89b64821a111076215
KMR-san <torayaura+github@gmail.com>
2025-06-19 20:20:51 +0900

    バックエンドの初期コミット

---
+++ ui_tracer/ui_tracer/src/main/java/com/example/ui_tracer/UiTracerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ui_tracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiTracerApplication.class, args);
+	}
+
+}
