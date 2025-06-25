f42d3a9b3ac82055719941302c5c3bd2e05c7b11
KMR-san <torayaura+github@gmail.com>
2025-06-25 23:13:57 +0900

    dディレクトリ構成の整理

--- ui_tracer/ui_tracer/src/main/java/com/example/ui_tracer/repository/IssueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ui_tracer.repository;
-
-import com.example.ui_tracer.entity.Issue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IssueRepository extends JpaRepository<Issue, Long> {
-}
